5a60dfdb8fb1a8e6190fab466716e433e968b2bb
Navnath-lakade-123 <ndlakade@gmail.com>
2025-07-01 18:42:24 +0530

    updated

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -11,7 +11,7 @@
 
     public void Mul()
     {
-        int a, b, c;
+        int a, b, c
         a = 5;
         b = 4;
         c = a * b;
--- FirstProject/obj/Debug/net8.0/FirstProject.AssemblyInfo.cs
+++ FirstProject/obj/Debug/net8.0/FirstProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff608d2ad1b8a2d4a990a21dc9b2b2d580b0cefe")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
