ff608d2ad1b8a2d4a990a21dc9b2b2d580b0cefe
Navnath-lakade-123 <ndlakade@gmail.com>
2025-07-01 18:18:44 +0530

    Initial commit

---
+++ FirstProject/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    public void Add()
+    {
+        int a, b, c;
+        a = 1;
+        b = 2;
+        c = a+ b;
+        Console.WriteLine("Addition="+c);
+    }
+
+    public void Mul()
+    {
+        int a, b, c;
+        a = 5;
+        b = 4;
+        c = a * b;
+        Console.WriteLine("Multiplication=" + c);
+    }
+    public static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Add();
+        program.Mul();
+        Console.ReadLine();
+    }
+}
